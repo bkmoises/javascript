f72929405875e23963f0c45b009523e1b9c855c7
bkmoises <bk_moises@hotmail.com>
2022-08-09 20:06:06 -0300

    Novas atualizações

---
+++ logica-algoritimos/first-if.js
@@ -0,0 +1,17 @@
+let nota = 10
+
+if (nota >= 9 && nota <= 10) {
+  console.log("Conceito A")
+}
+if (nota >= 9 && nota <= 10) {
+  console.log("Conceito B")
+}
+if (nota >= 9 && nota <= 10) {
+  console.log("Conceito C")
+}
+if (nota >= 9 && nota <= 10) {
+  console.log("Conceito D")
+}
+if (nota >= 9 && nota <= 10) {
+  console.log("Conceito F")
+}
